6393936e946c8b247ead4a8be3d3ec780e83f73c
Alaa Eddine <70238665+AlaaEddine20@users.noreply.github.com>
2020-10-11 15:14:40 +0200

    Add files via upload

--- DAY 4/M1-D4-Alaa.js
+++ DAY 4/M1-D4-Alaa.js
@@ -9,15 +9,18 @@ ASSIGNMENT RULES
 /* EXERCISE 1
 Write a function "area" which receives 2 parameters (l1,l2) and calculate the area of the rectangle.
 */
-
-//let l1 = 100;
-//let l2 = 50;
-//const area = (l1, l2) => l1 * l2;
-//console.log(area(60, 100))
+/**************************DONE**************************/
+/*
+const area = function(l1, l2){
+    return l1 * l2;
+}
+console.log(area(6, 10))
+*/
 
 /* EXERCISE 2
 Write a function "crazySum" which receives two given integers. If the two values are same, then returns triple their sum.
 */
+/**************************DONE**************************/
 /*
 function crazySum (x, y) {
     if (x === y) {
@@ -27,8 +30,8 @@ function crazySum (x, y) {
     }
     
 }
-
 console.log(crazySum(3, 3))
+
 */
 /* EXERCISE 3
 Write a function "crazyDiff" that computes the 
@@ -37,23 +40,21 @@ Returns triple their absolute difference if
 the specified
 number is greater than 19.
 */
+/**************************DONE**************************/
 /*
 function crazyDiff (givenNumber, nineTeen) {
-    let result;
     if (givenNumber < nineTeen){
-        result = "incorrect";
+        return "Not greater";
     } else if (givenNumber > nineTeen){
-        result = (givenNumber - nineTeen) * 3;
+        return (givenNumber - nineTeen) * 3;
     }
-    return result;
 };
-
 console.log(crazyDiff(20, 19));
 */
 /* EXERCISE 4
 Write a function "boundary" which accept an integer N and returns true if N is within 20 and 100 (included) or equal to 400.
 */
-
+/**************************DONE**************************/
 /* WRITE YOUR CODE HERE */
 /*
 function boundary (N) {
@@ -69,17 +70,19 @@ console.log(boundary(30))
 Write a function "strivify" which accepts a string S. Add to S "Strive" in front of a given string, if the given string begins with "Strive" then return the original string.
 */
 
-/* WRITE YOUR CODE HERE */
-
-let string = "S";
-function strivify(string){
-    
+/* WRITE YOUR CODE HERE */    // ASK TUTOR WHAT THIS TASK ASKS
+/*
+function strivify(S){
+    return S = "S" + "Strive";
 }
+console.log(strivify("c"))
+*/
 
 /* EXERCISE 6
 Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
 HINT: Module Operator
 */
+/**************************DONE**************************/
 /*
 function check3and7(X) {
     if (X % 3 === 0){
@@ -90,13 +93,14 @@ function check3and7(X) {
         return "No multiple of X Found";
     }
 }
-console.log(check3and7(15))
+console.log(check3and7(14))
 */
 /* WRITE YOUR CODE HERE */
 
 /* EXERCISE 7
 Write a function "reverseString" to reverse programmatically a given string (es.: Strive => evirtS).
 */
+/**************************DONE**************************/
 /*
 function reverseString(myString){
       return myString.split("").reverse().join("");
@@ -108,12 +112,12 @@ console.log(reverseString("strive"))
 /* EXERCISE 8
 Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as parameter
 */
+/**************************DONE**************************/    // NOT CLEAR
 /*
 const firstLetter = "Sono stupido davvero"
 function upperFirst(firstLetter){
     return firstLetter.split(' ').map(function (s) { return s.charAt(0); }).join('');
-}
-
+};
 console.log(upperFirst(firstLetter))
 */
 /* WRITE YOUR CODE HERE */
@@ -121,6 +125,7 @@ console.log(upperFirst(firstLetter))
 /* EXERCISE 9
 Write a function "cutString" to create a new string without the first and last character of a given string.
 */
+/**************************DONE**************************/
 /*
 const myString = "n It Works n"
 function cutString(myString){
@@ -133,14 +138,15 @@ console.log(cutString(myString))
 /* EXERCISE 10
 Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10
 */
-const n_length = 10;
-const randomNarray = [];
+/**************************DONE**************************/
+/*
 function giveMeRandom(randomNarray){
-    for (let i = 0; i<n_length; i++){
-        randomNarray.push(Math.random())
+    for (let i = 0; i<10; i++){
+        return randomNarray = [Math.floor(Math.random() * 10)]
     }
 };
-
+console.log(giveMeRandom())
+*/
 /* WRITE YOUR CODE HERE */
 
 /* WHEN YOU ARE FINISHED
