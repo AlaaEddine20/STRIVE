ae330fc52b09ceda4fabd249361d3f9fa5c89f62
Alaa Eddine <alaneddin91@gmail.com>
2020-10-14 22:03:05 +0200

    M1-D5 Ex.9 DONE

--- MODULE-1/DAY 7 - FRIDAY-CHALLENGE/M1-D5.js
+++ MODULE-1/DAY 7 - FRIDAY-CHALLENGE/M1-D5.js
@@ -13,25 +13,26 @@
 
 /* Ex.A
    Create a variable test that contains a string
-*/
-//const test = "First friday challenge";
+*///////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
+/*
+const test = "First friday challenge";
 /* Ex.B
     Create a variable sum that contains the result of the sum between 10 and 20 
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 let sum = 10 + 20;
 console.log(sum)
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /* Ex.C 
     Create a variable random that contains a random number between 0 and 20 (should be randomly created at each execution)
 */
 /*
 let random = Math.floor(Math.random() * 20);
 console.log(random)
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /* Ex.D
     Create a variable Me containing and object with the current information: Name = Your Name, Surname = Your Surname, Age = Your Age
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 const Me = [
 {
@@ -43,20 +44,20 @@ const Me = [
 */
 /* Ex.E 
     Programmatically remove the Age from the previously create object Me
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 
 //delete Me.age;
 //console.log(Me)
 
 /* Ex.F 
    Programmatically add to the object Me an array "skills" that contains the programming languages that you know
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 //Me["Skills"] = "Javascript";
 //Object.assign(Me, {Skills: "JS"});
 //console.log(Me)
 /* Ex.G 
    Programmatically remove the last skill from the array "skills" inside of the "me" object
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 delete Me.Skills;
 console.log(Me)
@@ -64,16 +65,16 @@ console.log(Me)
 // JS Functions
 /* Ex.1
     Write the function Dice that randomize an integer number between 1 and 6
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 function Dice(n){
   return Math.floor(Math.random() * 6) + 1
 }
 console.log(Dice())
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /* Ex.2 
     Write the function WhoIsBigger that receives 2 numbers and returns the bigger of the 2
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 function WhoIsBigger(x, y){
   return Math.max(x, y)
@@ -84,8 +85,7 @@ console.log(WhoIsBigger(5, 4))
 /* Ex.3
     Write the function SplitMe that receives a String and returns an array with every word in that string
     Ex. SplitMe("I love coding") => returns [ "I","Love","Coding"]
-*/
-
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 function SplitMe (string){
       return string.trim().split(" ");
@@ -94,34 +94,25 @@ console.log(SplitMe("Let see if it works"))
 */
 /* Ex.4
     Write the function DeleteOne that receives a string and a boolean. If the boolean is true, should return the string without the first letter, otherwise should remove the last one
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 let string = "abcdefg";
 let boolean = 10;
 function DeleteOne(string, boolean){
-  if (boolean > 1){
+  if (boolean > 5){
     return string.slice(1); // IF I WANT THE FIRST CONDITION TO BE FALSE?
   } else {
     return string.slice(0, -1); // AND THE SECOND ONE TO BE TRUE?
   }
 }
-console.log(DeleteOne(string, boolean));
+console.log(DeleteOne(string, 1));
 */
 /* Ex.5
    Write the function OnlyLetters that receives a string, removes all the numbers and returns it.
    Ex.: OnlyLetters("I love 123 whatever")  => returns "I love whatever"
-*/
-// FIRST METHOD NOT FINISHED
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
-const string = "i love 129 whatever";
-function OnlyLetters(string){
-  return string.split(" ");
-}
-*/
-
-// SECOND METHOD 
-/*
-function OnlyLetters(string){
+function OnlyLetters(string){                      /////// IS IT CORRECT?! 
   if (string.match(1, 2, 3, 4, 5, 6, 7, 8, 9)){
     return "Numbers not allowed";
   } else {
@@ -133,7 +124,7 @@ console.log(OnlyLetters("i love 132 whatever"))
 */
 /* Ex.6 
    Write the function IsThisAnEmail that receives a string and returns true if the string is a valid email.
-*/
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 function IsThisAnEmail(string){
   if (string.match("@")){
@@ -146,8 +137,7 @@ console.log(IsThisAnEmail("merdaemail.it"))
 */
 /* Ex.7
    Write the function WhatDayIsIt that should return the day of the week
-*/
-// DONE
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
 let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"];
 let today = new Date().getDay();
@@ -174,15 +164,15 @@ function RollTheDices(num){
 */
 /* Ex.9
    Write the function HowManyDays that receives a Date and return the number of days that has passed since that day.
-*/
-//let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday"];
-//let date = new Date("09/30/2020");
+*////////////////////////////////////////////////////DONE////////////////////////////////////////////////////////
 /*
-let daysPassed = Date.parse("10/04/2020");
-function howManyDays(daysPassed){
-    return daysPassed 
+function howManyDays() {
+  let today = new Date();
+  let otherDay = new Date(2020, 8, 14);
+  let oneDay = 1000 * 60 * 60 * 24;
+  return Math.round((today - otherDay)/ oneDay)
 }
-console.log(howManyDays(daysPassed))
+console.log(howManyDays())
 */
 /* Ex.10
    Write the function IsTodayMyBDay that returns true if it's your birthday, false otherwise
@@ -267,10 +257,9 @@ console.log(obj.Title)
 */
 /*
 function onlyThisMillenium(){
-  for (let i=0; i<movies.length; i++){
-    parseInt(movies.Year)
-    if (movies[i].Year>2000);{
-      console.log(movies[i]);
+  for (let i=0; i < movies.length; i++){    
+    if (parseInt(movies.Year)>2000);{
+      console.log(movies[i].Year);
     }
   }
 }
@@ -296,9 +285,9 @@ console.log(onlyThisMillenium())
    Write the function DeleteX that receives a number and returns an array without the element in that position
 */
 /*
-let arr = ["one", "two", "three"]
-function deleteX () {
-  return [arr.slice([1])]
+function deleteX() {
+  let arr = [0, 1, 2, 3]
+  return [arr.slice([3])]
 }
 console.log(deleteX())
 */
@@ -469,9 +458,11 @@ const movies = [
     Poster:
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
-];
-/*
+]
+
+
 // EX 16
+/*
 let iD = "tt4154796";
 function getMovieById(iD){
 for (i = 0; i < movies.length; i++) {
@@ -491,6 +482,7 @@ function searchMovie(){
 }
 console.log(searchMovie())
 */
+/*
 let string = "Lord of";
 function searchMovie(string) { 
   for (i=0; i<movies.length; i++){
@@ -498,3 +490,4 @@ function searchMovie(string) {
   }
 }
 console.log(searchMovie(string));
+*/
