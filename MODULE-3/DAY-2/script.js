0da6de2dda0a575d66e1335bda30f3f57be09341
Alaa Eddine <alaneddin91@gmail.com>
2020-11-02 13:29:50 +0100

    adds ex 1-2-3

---
+++ MODULE-3/DAY-2/script.js
@@ -0,0 +1,39 @@
+
+// EXERCISE 1
+/*
+const calculateSumOfInteg = function() {
+    let integOne = 4
+    let integTwo = 4
+    let sum = integOne + integTwo
+    if (integOne === integTwo) {
+        return 3*(sum)
+    } else {
+        return "not the same"
+    }
+}
+
+*/
+
+// EXERCISE 2
+/*
+const checkInteg = function() {
+    let integOne = 25
+    let integTwo = 25
+    let sum = integOne + integTwo
+    if ((integOne === 50 || integTwo === 50) || (sum === 50)) {
+        return true
+    } else {
+        return false
+    }
+}
+*/
+
+// EXERCISE 3
+/*
+const removeAndReturnString = function() {
+    let string = "remove a character and return new string"
+    let newStr = string.replace('a', '');
+    console.log(newStr)
+}
+removeAndReturnString()
+*/
