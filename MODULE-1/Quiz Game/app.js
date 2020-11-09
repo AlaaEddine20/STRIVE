92e6d72c0fb15308915c1013720214d29d9733bc
Alaa Eddine <alaneddin91@gmail.com>
2020-11-09 22:45:11 +0100

    fetch + DOM, almost there

--- MODULE-1/Quiz Game/app.js
+++ MODULE-1/Quiz Game/app.js
@@ -1,20 +1,36 @@
-const questionsAndAnswers = 
-    {
-        question: "Wich of these variables is a string?",
-        choice1: "let variable = ciao",
-        choice2: "let variable = 12",
-        choice3: "let variable = /i am a string/",
-        choice4: "let variable = 'i am a string'",
-        answer: 4
-    }
 
 
-const question = document.querySelector(".question").textContent = questionsAndAnswers.question
+const question = document.querySelector(".question")
+
+ 
+let choices = document.querySelectorAll(".choice-text")
+let choice1 = document.querySelector("#A")
+let choice2 = document.querySelector("#B")
+let choice3 = document.querySelector("#C")
+let choice4 = document.querySelector("#D")
 
 
-let choices = document.querySelectorAll(".choice")
-let choice1 = document.querySelector("#A").textContent = questionsAndAnswers.choice1
-let choice2 = document.querySelector("#B").textContent = questionsAndAnswers.choice2
-let choice3 = document.querySelector("#C").textContent = questionsAndAnswers.choice3
-let choice4 = document.querySelector("#D").textContent = questionsAndAnswers.choice4
+fetch("https://opentdb.com/api.php?amount=5&category=11")
+.then( response => response.json() )
+.then( function (data) {
+    data.results.forEach(el => {
+        // console.log(data)
+        let questions = el.question.replace(/&amp;/g, "&").replace(/&gt;/g, ">").replace(/</g, "&lt;").replace(/&quot;/g, '"');
+        
+       
+        question.textContent = questions
+        data.results.forEach(answer => {
+            console.log("INCORRECT: ",answer.incorrect_answers)
+            console.log("CORRECT: ",answer.correct_answer)
+            let correctAnswer = answer.correct_answer
+            choices[Math.floor(Math.random() * 3)].textContent = correctAnswer
+            
+            let wrongAnswers = answer.incorrect_answers
+            choices[Math.floor(Math.random() * 3) + 1].textContent = wrongAnswers
 
+        })
+            
+        
+        
+    });
+} )
