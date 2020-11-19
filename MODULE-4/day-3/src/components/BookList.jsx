c577dced33e70922fd88fba200fa82606adad53e
Alaa Eddine <alaneddin91@gmail.com>
2020-11-19 15:40:14 +0100

    categories filtered

--- MODULE-4/day-3/src/components/BookList.jsx
+++ MODULE-4/day-3/src/components/BookList.jsx
@@ -1,25 +1,66 @@
-import React from 'react'
-import books from '../data/horror.json'
-import { Card, Container, Row, Col } from 'react-bootstrap';
-import SingleBook from './SingleBook';
+import React from "react";
+import horrorBooks from "../data/horror.json";
+import historyBooks from "../data/history.json";
+import romanceBooks from "../data/romance.json";
+import scifiBooks from "../data/scifi.json";
+import fantasyBooks from "../data/fantasy.json";
+import { Container, Row, Dropdown, Form } from "react-bootstrap";
+import SingleBook from "./SingleBook";
 
-const BookList = () => {
-  
+class BookList extends React.Component {
+  state = {
+    allBooks: [
+      ...horrorBooks,
+      ...historyBooks,
+      ...romanceBooks,
+      ...scifiBooks,
+      ...fantasyBooks,
+    ],
+    filteredBooks: [],
+  };
+
+  render() {
     return (
-      <Container>
-          <Row>
-                {books.map((book,key) => {
-                   
-                    return (
-
-                         <SingleBook data={book} key={key}  /> 
-                       
-                    )
-                }
-                )}
-         </Row>
-         </Container>
-    )
+      <Container
+        fluid
+        style={{ backgroundColor: "black" }}
+        className="d-flex justify-content-center"
+      >
+        <Form>
+          <Form.Group controlId="exampleForm.SelectCustom">
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              as="select"
+              custom
+              onChange={(e) =>
+                this.setState({
+                  filteredBooks: this.state.allBooks.filter(
+                    (book) =>
+                      book.category === e.currentTarget.value.toLowerCase()
+                  ),
+                })
+              }
+            >
+              <option>Fantasy</option>
+              <option>Horror</option>
+              <option>Romance</option>
+              <option>History</option>
+              <option>Scifi</option>
+            </Form.Control>
+          </Form.Group>
+        </Form>
+        <Row className="text-center d-flex">
+          {this.state.filteredBooks.length > 0
+            ? this.state.filteredBooks.map((book, key) => {
+                return <SingleBook data={book} key={key} />;
+              })
+            : this.state.allBooks.map((book, key) => {
+                return <SingleBook data={book} key={key} />;
+              })}
+        </Row>
+      </Container>
+    );
+  }
 }
 
-export default BookList
+export default BookList;
--- MODULE-4/day-3/src/components/SingleBook.jsx
+++ MODULE-4/day-3/src/components/SingleBook.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Col, Container } from 'react-bootstrap';
-import books from '../data/horror.json'
+import { Card, Col } from 'react-bootstrap';
 
 
 const SingleBook = (props) => {
 
     return (
+   
         <Col>
-        <Card>
-        
-            <Card.Img src={props.data.img} style={{ width: 400, height: 500 }} />
-           
-        </Card>
+         <Card style={{ width: 300, height: 400, marginBottom: '1rem', backgroundColor: 'orange' }}>
+           <Card.Title style={{ fontSize: '1rem' }}>
+               {props.data.title}
+           </Card.Title>
+           <Card.Img className="mx-auto my-auto" 
+           src={props.data.img} 
+           style={{ width: 200, height: 330, objectFit: 'cover' }}
+           />
+         </Card>
         </Col>
+             
     )
 }
     
