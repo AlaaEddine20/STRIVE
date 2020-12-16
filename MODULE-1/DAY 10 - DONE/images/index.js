1bc589f241cea888e7de3d33dab680eb88b5b817
Alaa Eddine <alaneddin91@gmail.com>
2020-12-16 15:34:36 +0100

    moving folders

--- MODULE-1/DAY 10 - DONE/images/index.js
+++
@@ -1,3 +0,0 @@
-let x = 'John';
-let y = 'Doe'
-console.log(x + y)
--- MODULE-3/DAY-3/.vscode/script.js
+++
@@ -1,6 +0,0 @@
-window.onload = () => {
-    const loadImages = fetch("http://www.splashbase.co/api/v1/images/1")
-    .then(response).then(response => response.json()).then(console.log(loadImages))
-}
-
-console.log("Hello")
